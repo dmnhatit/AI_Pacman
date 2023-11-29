dcba5ff5a53b38b0d6178dc44d592f75ee70da48
TruongThinhcoder <117327613+TruongThinhcoder@users.noreply.github.com>
2023-11-29 23:00:44 +0700

    codeidsdfs2

--- algorithm/dfs_algorithm.py
+++ algorithm/dfs_algorithm.py
@@ -1,9 +1,7 @@
-#%% 
+#%%
+
 from collections import deque
-import heapq
-
 class Node:
-
     def __init__(self, state, parent=None, cost=0):
         self.state = state
         self.parent = parent
@@ -15,71 +13,30 @@ class Node:
             path_back.append(node.state)
             node = node.parent
         return list(reversed(path_back))
-    
+
     def path_cost(self):
         node, total_cost = self, 0
         while node.parent:
             total_cost += node.cost
             node = node.parent
         return total_cost
-    
+
     def get_directions(self):
         path = self.get_path()
         directions = []
         for i in range(1, len(path)):
-            dx = path[i][0] - path[i-1][0]
-            dy = path[i][1] - path[i-1][1]
+            dx = path[i][0] - path[i - 1][0]
+            dy = path[i][1] - path[i - 1][1]
             if dx == 1:
-                directions.append('DOWN')
+                directions.append("DOWN")
             elif dx == -1:
-                directions.append('UP')
+                directions.append("UP")
             elif dy == 1:
-                directions.append('RIGHT')
+                directions.append("RIGHT")
             elif dy == -1:
-                directions.append('LEFT')
+                directions.append("LEFT")
         return directions
- 
-class PriorityQueue:
-    def __init__(self):
-        self.elements = []  # list các phần từ được duy trì thứ tự heapq
-        self.entry_finder = (
-            {}
-        )  # sử dụng để theo dõi vị trí của từng phần tử trong self.elements.
-        self.REMOVED = (
-            "<removed-task>"  # thằng số đánh dấu phần tử bị loại bỏ khỏi hàng đợi.
-        )
-        self.counter = 0  # biến sử dụng để đảm bảo sự duy nhất
 
-    # kiểm tra hàng đợi có đang rỗng
-    def empty(self):
-        return not self.entry_finder
-
-    # thêm item với mức độ ưu tiên vào hàng đợi. Nếu item đã tồn tại thì loại bỏ item đó để thay bằng item mới
-    def put(self, item, priority):
-        if item in self.entry_finder:
-            self.remove(item)
-        entry = [priority, self.counter, item]
-        self.counter += 1
-        self.entry_finder[item] = entry
-        heapq.heappush(self.elements, entry)
-
-    # Loại bỏ item khỏi dict entry_finder và đánh dấu remove trong self.elements
-    def remove(self, item):
-        entry = self.entry_finder.pop(item)
-        entry[-1] = self.REMOVED
-
-    # lấy item ra khỏi hàng đợi và xoá nó khỏi dict entry_finder
-    def get(self):
-        while self.elements:
-            priority, count, item = heapq.heappop(self.elements)
-            if item is not self.REMOVED:
-                del self.entry_finder[item]
-                return item
-        raise KeyError("get from an empty priority queue")
-
-    # được sử dụng để kiểm tra xem một phần tử có tồn tại trong hàng đợi ưu tiên hay không
-    def __contains__(self, item):
-        return item in self.entry_finder       
 
 class Problem:
     def __init__(self, maze, initial_state, goal_state):
@@ -100,9 +57,10 @@ class Problem:
             for x, y in successors
             if 0 <= x < len(self.maze)
             and 0 <= y < len(self.maze[0])
-            and self.maze[x][y] != 1
+            and self.maze[x][y] != 0
         ]
         return successors
+
 
 def dfs(problem):
     start_node = Node(problem.initial_state)
@@ -110,43 +68,66 @@ def dfs(problem):
         return start_node
     frontier = [start_node]
     explored = set()
+    explored.add(start_node.state)
     while frontier:
         node = frontier.pop()
-        explored.add(node.state)
         for child_state in problem.get_successors(node.state):
             child = Node(child_state, node, node.cost + 1)
             if child.state not in explored and child not in frontier:
+                explored.add(child.state)
                 if problem.goal_test(child.state):
                     return child
                 frontier.append(child)
     return None
 
-def ids(problem, max_depth  = 1):
-    start_node = Node(problem.initial_state)
-    if problem.goal_test(start_node.state):
-        return start_node
-    state_eq_limit = [start_node]
-    explored = set()
-    explored.add(start_node.state)
-    while state_eq_limit:
-        stack = state_eq_limit
-        state_eq_limit = []
-        while stack:
-            node = stack.pop()
-            for child_state in problem.get_successors(node.state):
-                child = Node(child_state, node, node.cost + 1)
-                if child.state not in explored and child not in stack:
-                    explored.add(child.state)
-                    if problem.goal_test(child.state):
-                        return child
-                    stack.append(child)
-                    if child.cost == max_depth:
-                        state_eq_limit.append(child)
-        max_depth = max_depth + 1
-    return None
+def ids(problem):
+    depth = 0
+    visited = []
+    while depth < 100:
+        result = dls(problem, depth, visited)
+        if result != -1:
+            return result
+        depth += 1
+        visited = []
 
 
-if __name__ == '__main__':
+def dls(problem, limit, visited=[]):
+    return recursive_dls(Node(problem.initial_state), problem, limit, visited)
+
+
+def recursive_dls(node, problem, limit, visited):
+    visited.append(node)
+    if problem.goal_test(node.state):
+        return node
+    elif limit == 0:
+        return -1  # cutoff
+    else:
+        cutoff_occurred = False
+        for child_state in problem.get_successors(node.state):
+            child = Node(child_state, node, node.cost + 1)
+            if not_in_visited(visited, child_state) or in_but_better(visited, child):
+                result = recursive_dls(child, problem, limit - 1, visited)
+                if result == -1:  # cutoff
+                    cutoff_occurred = True
+                elif result is not None:
+                    return result
+        if cutoff_occurred:
+            return -1  # cutoff
+        else:
+            return None  # failure
+
+def not_in_visited(visited, child_state):
+    return child_state not in [node.state for node in visited]
+
+def in_but_better(visited, node):
+    for visited_node in visited:
+        if visited_node.state == node.state and visited_node.cost > node.cost:
+            visited.remove(visited_node)
+            return True
+    return False
+
+
+if __name__ == "__main__":
     maze = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0],
@@ -174,25 +155,27 @@ if __name__ == '__main__':
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ]
 
-    problem = Problem(maze, (1, 1), (16, 22))
-    node_dfs = dfs(problem)
+
+    # problem = Problem(maze2, (0, 0), (8, 9))
+    problem = Problem(maze, (1, 1), (16, 21))
     node_ids = ids(problem)
+    node_dfs = dfs(problem)
+    
+    if node_ids is not None:
+        print(node_ids.get_directions())
+        print(node_ids.cost)
+    else:
+        print("no solution")
+
+    node_dfs = dfs(problem)
     if node_dfs is not None:
         print(node_dfs.get_directions())
         print(node_dfs.cost)
     else:
         print("No solution")
 
-    if node_ids is not None:
-        print(node_ids.get_directions())
-        print(node_ids.cost)
-    else:
-        print("No solution")
-
     if node_ids is not None and node_dfs is not None:
         if node_dfs.get_directions() == node_ids.get_directions():
             print("same")
-
-
 
 # %%
